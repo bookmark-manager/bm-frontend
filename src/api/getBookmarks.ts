6477b9828988ee7bc7763b98fcf157f4ca4cbe40
abdugaffor2004 <abdughaffar7104@gmail.com>
2025-09-09 17:15:52 +0300

    refactor: replace pagination search params to custom headers

--- src/api/getBookmarks.ts
+++ src/api/getBookmarks.ts
@@ -11,16 +11,16 @@ export interface ListOptions {
 }
 
 export const getBookmarks = async (opts: ListOptions): Promise<PaginatedResponse<Bookmark>> => {
-  const url = getBookmarksApiUrl();
-  const params = url.searchParams;
+  const headers = new Headers();
+  headers.append('X-PerPage', opts.perPage.toString());
+  headers.append('X-Page', opts.page.toString());
 
-  params.set('per_page', opts.perPage.toString());
-  params.set('page', opts.page.toString());
+  const url = getBookmarksApiUrl();
   if (opts.search !== '') {
-    params.set('search', opts.search);
+    url.searchParams.set('search', opts.search);
   }
 
-  return fetch(url)
+  return fetch(url, { headers })
     .then(resp => resp.json())
     .then(data => {
       return fromPaginatedResponseDto(data, fromBookmarkDto);
